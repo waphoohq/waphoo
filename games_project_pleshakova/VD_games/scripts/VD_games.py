326f2d0f171a1a3f5ca3065f1c4c6f0613df7f84
waphoohq <waphoohq.com>
2026-03-19 02:02:23 +0300

    практа 2

---
+++ games_project_pleshakova/VD_games/scripts/VD_games.py
@@ -0,0 +1,5 @@
+def main():
+	print("Welcome to the VD_games")
+
+if __name__ == "__main__":
+	main()
---
+++ games_project_pleshakova/VD_games/scripts/VD_main.py
@@ -0,0 +1,4 @@
+from games_project_pleshakova.VD_games.scripts.VD_games import main
+
+if __name__ == "__main__":
+	main()
